c24bffcd355b81e3a9d0e3ea4aa8c42c356568e5
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-12-01 15:26:48 +0100

    exit program, add a box with border

--- cli.js
+++ cli.js
@@ -7,3 +7,33 @@ const screen = blessed.screen({
 })
 
 screen.title = 'game-terminal'
+
+const box = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: '50%',
+  height: '50%',
+  content: 'Hello {bold}world{/bold}!',
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: 'magenta',
+    border: {
+      fg: '#f0f0f0'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }
+})
+
+screen.append(box)
+
+screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+  return process.exit(0)
+})
+
+screen.render()
