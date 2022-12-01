b7dac600a77dd4c586d8edfc64fa3afc7b10594b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-12-01 19:38:22 +0100

    remove demo code

--- cli.js
+++ cli.js
@@ -13,30 +13,6 @@ const screen = blessed.screen({
 // Disable on VT320 - will irritate screen
 // screen.title = 'game-terminal'
 
-const box = blessed.box({
-  top: 'center',
-  left: 'center',
-  width: '50%',
-  height: '50%',
-  content: 'Hello {bold}world{/bold}!',
-  tags: true,
-  border: {
-    type: 'line'
-  },
-  style: {
-    fg: 'white',
-    bg: 'magenta',
-    border: {
-      fg: '#f0f0f0'
-    },
-    hover: {
-      bg: 'green'
-    }
-  }
-})
-
-screen.append(box)
-
 screen.key(['escape', 'q', 'C-c'], function (ch, key) {
   return process.exit(0)
 })
