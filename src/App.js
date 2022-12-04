8bfd44c93c55114dd33bca78b9aa29f3f48697b7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-12-04 08:25:31 +0100

    App: terminal mode

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const blessed = require('reblessed')
+
 module.exports = class App {
   constructor (id, data) {
     this.id = id
@@ -9,7 +11,44 @@ module.exports = class App {
   }
 
   exec (screen) {
-    screen.setTerminal(screen.program._terminal)
-    screen.spawn(this.data.command, [], { stdio: 'inherit' })
+    if (this.data.terminal) {
+      const height = this.data.instructions.split(/\n/).length - 1
+      const instructions = blessed.box({
+        top: 0,
+        left: 0,
+        right: 0,
+        height,
+        content: this.data.instructions
+      })
+      screen.append(instructions)
+
+      const line = blessed.Line({
+        top: height,
+        left: 0,
+        right: 0,
+        orientation: 'horizontal'
+      })
+      screen.append(line)
+
+      const term = blessed.terminal({
+        parent: screen,
+        label: 'test',
+        top: height + 1,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        cursor: 'block'
+      })
+      term.focus()
+      term.on('exit', () => {
+        term.destroy()
+        line.destroy()
+        instructions.destroy()
+        screen.render()
+      })
+    } else {
+      screen.setTerminal(screen.program._terminal)
+      screen.spawn(this.data.command, [], { stdio: 'inherit' })
+    }
   }
 }
