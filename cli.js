6ef026adaa12556ce77abdb8ea934d1f5ad93ce0
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-12-04 21:10:47 +0100

    load other stuff after recode-init

--- cli.js
+++ cli.js
@@ -3,9 +3,6 @@ const blessed = require('reblessed')
 const Pager = require('./src/Pager')
 const loadList = require('./src/loadList')
 const recode = require('./src/recode')
-recode.init((err) => {
-  if (err) { console.error(err) }
-})
 
 // Create a screen object.
 const screen = blessed.screen({
@@ -15,21 +12,29 @@ const screen = blessed.screen({
 // Disable on VT320 - will irritate screen
 // screen.title = 'game-terminal'
 
-loadList()
+recode.init((err) => {
+  if (err) { console.error(err) }
+
+  init()
+})
+
+function init () {
+  loadList()
   .then(list => {
-    //    if (err) {
-    //      console.log(err)
-    //      process.exit(1)
-    //    }
+      //    if (err) {
+      //      console.log(err)
+      //      process.exit(1)
+      //    }
 
-    const pager = new Pager({
-      list,
-      fields: [
-        { pager: true, title: 'title' }
-      ],
-      screen
+      const pager = new Pager({
+        list,
+        fields: [
+          { pager: true, title: 'title' }
+        ],
+        screen
+      })
+      pager.show()
+
+      screen.render()
     })
-    pager.show()
-
-    screen.render()
-  })
+}
