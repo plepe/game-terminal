e46b36191ff78131b302899d9f81682efc814480
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-12-04 10:02:56 +0100

    Always start apps as terminal windows

--- src/App.js
+++ src/App.js
@@ -11,44 +11,44 @@ module.exports = class App {
   }
 
   exec (screen) {
-    if (this.data.terminal) {
-      const height = this.data.instructions.split(/\n/).length - 1
-      const instructions = blessed.box({
-        top: 0,
-        left: 0,
-        right: 0,
-        height,
-        content: this.data.instructions
-      })
+    const height = this.data.instructions ? this.data.instructions.split(/\n/).length - 1 : 0
+
+    const instructions = blessed.box({
+      top: 0,
+      left: 0,
+      right: 0,
+      height,
+      content: this.data.instructions
+    })
+
+    const line = blessed.Line({
+      top: height,
+      left: 0,
+      right: 0,
+      orientation: 'horizontal'
+    })
+
+    if (height) {
       screen.append(instructions)
-
-      const line = blessed.Line({
-        top: height,
-        left: 0,
-        right: 0,
-        orientation: 'horizontal'
-      })
       screen.append(line)
-
-      const term = blessed.terminal({
-        parent: screen,
-        label: 'test',
-        top: height + 1,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        cursor: 'block'
-      })
-      term.focus()
-      term.on('exit', () => {
-        term.destroy()
-        line.destroy()
-        instructions.destroy()
-        screen.render()
-      })
-    } else {
-      screen.setTerminal(screen.program._terminal)
-      screen.spawn(this.data.command, [], { stdio: 'inherit' })
     }
+
+    const term = blessed.terminal({
+      parent: screen,
+      label: 'test',
+      top: height + 1,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      shell: this.data.command || '/bin/bash',
+      cursor: 'block'
+    })
+    term.focus()
+    term.on('exit', () => {
+      term.destroy()
+      line.destroy()
+      instructions.destroy()
+      screen.render()
+    })
   }
 }
